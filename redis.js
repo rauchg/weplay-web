7ed63b05bf45ba3b5948f43714703bbd7538dc01
Jiahao Dai <20123225@student.shiep.edu.cn>
2015-10-12 18:21:15 +0800

    Fix redis client check ready glitch

--- redis.js
+++ redis.js
@@ -1,11 +1,8 @@
-
 var redis = require('redis');
 var uri = process.env.WEPLAY_REDIS_URI || 'localhost:6379';
 var auth_password = process.env.WEPLAY_REDIS_AUTH || '';
 var pieces = uri.split(':');
 
 module.exports = function(){
-  var client =  redis.createClient(pieces[1], pieces[0], { return_buffers: true });
-  client.auth(auth_password);
-  return client;
+  return redis.createClient(pieces[1], pieces[0], { return_buffers: true, no_ready_check: true, auth_pass: auth_password });
 };
