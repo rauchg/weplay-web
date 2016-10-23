0c02377f09408ce9b3acc73a9546d0c7ba27eb15
Iván Rodríguez Murillo <wantez@gmail.com>
2016-10-23 03:18:27 +0200

    redis logging

--- redis.js
+++ redis.js
@@ -3,5 +3,14 @@ var uri = process.env.WEPLAY_REDIS_URI || 'localhost:6379';
 var pieces = uri.split(':');
 
 module.exports = function () {
-  return redis.createClient(pieces[1] || 6379, pieces[0], {return_buffers: true});
+  var host = pieces[0];
+  var port = pieces[1] || 6379;
+  var redisClient = redis.createClient(port, host, {return_buffers: true})
+  redisClient.on('connect', function () {
+    console.log('Redis connected to ' + host + ':' + port);
+  });
+  redisClient.on('error', function (err) {
+    console.error('Redis error ', err);
+  });
+  return redisClient;
 };
